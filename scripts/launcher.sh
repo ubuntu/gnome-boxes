eea12e757ed24e31e921cad7849c81b91b31a063
Sergio Costas <sergio.costas@canonical.com>
2023-07-05 16:03:45 +0200

    Updated to Core22 (#12)
    * Updated to Core22
    
    This patch updates the snap to Core22. Since it requires a Qemu
    version for Core22 too, it uses temporarily "qemu-virgil-tmp-work"
    until "qemu-virgil" is updated to Core22.
    
    * Added version for libvirt-glib and other bits
    
    * Add missing tags

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -11,9 +11,12 @@ export HOME="/home/$USER/snap/$SNAP_NAME/current"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 export PATH="/usr/bin:$PATH"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/usr/lib/x86_64-linux-gnu/ceph"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/usr/lib/x86_64-linux-gnu/ceph:$SNAP/usr/lib:$SNAP/lib:$SNAP/lib/x86_64-linux-gnu:$SNAP/usr/lib/x86_64-linux-gnu"
 
+echo Launching libvirtd
 libvirtd -d -p /tmp/libvirt.pid
+
+echo Launching virtlogd
 virtlogd -d -p /tmp/virtlogd.pid
 
 "$@"
